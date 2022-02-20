0a7c2eccf7fd4e700fd18bc7e7f8cf4ce89a14c1
herzliyaaa <herzliabarangan@gmail.com>
2022-02-20 17:46:57 +0800

    added spinner

--- sign-up.php
+++ sign-up.php
@@ -4,11 +4,12 @@ $showAlert = false;
 $showError = false;
 $exists = false;
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Include file which makes the
     // Database Connection.
-    include 'config.php';
+    include 'db/config.php';
 
     $username = $_POST["username"];
     $email = $_POST["email"];
@@ -98,44 +99,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     <div class="wrapper">
-        <div class="logo"> <img src="img/NDDUseal.png" alt=""> </div>
+        <div class="logo"> <img src="assets/img/NDDUseal.png" alt=""> </div>
         <div class="text-center mt-4 name"> Notre Dame of Dadiangas University </div>
         <form class="p-3 mt-3" action="sign-up.php" method="post">
             <div class="form-field d-flex align-items-center">
                 <span class="far fa-user"></span>
-                <input type="text" name="email" id="email" placeholder="Email">
+                <input type="text" name="email" id="email" placeholder="Email" required>
             </div>
             <div class="form-field d-flex align-items-center">
                 <span class="far fa-user"></span>
-                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="text" name="username" id="username" placeholder="Username" required>
             </div>
             <div class="form-field d-flex align-items-center">
                 <span class="fas fa-key"></span>
-                <input type="password" name="password" id="pwd" placeholder="Password">
+                <input type="password" name="password" id="pwd" placeholder="Password" required>
             </div>
             <div class="form-field d-flex align-items-center">
                 <span class="fas fa-key"></span>
-                <input type="password" name="cpassword" id="pwd" placeholder="Confirm Password">
+                <input type="password" name="cpassword" id="pwd" placeholder="Confirm Password" required>
             </div>
             <button class="btn mt-3" type="submit">Signup</button>
         </form>
         <div class="text-center fs-6"> Already have an account? <a href="index.php">Login</a> </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="
-https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="
-sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
 
     <script src="
-https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
 
     <script src="
-https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script>
 </body>
 
