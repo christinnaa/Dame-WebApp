b8943f36c8f8b33bd468d5fbaa226ece274dc334
herzliyaaa <herzliabarangan@gmail.com>
2022-03-02 13:29:45 +0800

    fixed nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,15 @@ $(window).on('load', function() {
 });
 
 // PRINT
-
+function printPage() {
+    //Get the print button and put it into a variable
+    var printBtn = document.getElementById("print-btn");
+    var backBtn = document.getElementById("back-btn");
+    //Set the buttons visibility to 'hidden'
+    printBtn.style.visibility = "hidden";
+    backBtn.style.visibility = "hidden";
+    //Print the page content
+    window.print();
+    printBtn.style.visibility = "visible";
+    backBtn.style.visibility = "visible";
+  }
